bccfd46594246a0e77db28e268e8124b01ac2639
Raquel <raquel.sales@escolar.ifrn.edu.br>
2025-06-27 11:02:16 -0300

    rascunho rodape

--- src/lib/componentes/Rodape.svelte
+++ src/lib/componentes/Rodape.svelte
@@ -1,1 +1,24 @@
-<h1>Rodapé</h1>
+<footer class="footer">
+  <div class="footer-container">
+    <div class="footer-section">
+      <h3>Biblioteca</h3>
+      <p>frasefrasefrasefrasefrasefrasefrase.</p>
+    </div>
+
+    <div class="footer-section">
+      <h4>Links</h4>
+      <ul>
+        <li><a href="/catalogo">Catálogo</a></li>
+        <li><a href="/contato">Contato</a></li>
+        <li><a href="/sobre">Sobre nós</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-section">
+      <h4>Contato bla bla</h4>
+      <p>Email: atendimento@biblioteca.com</p>
+      <p>Telefone: 8888888888</p>
+      <p>Endereço: Rua IFRN jsdllda</p>
+    </div>
+  </div>
+</footer>
