37ab38fa52234744ada3de581a01b27254dfcb2f
Salesr4 <161023393+Salesr4@users.noreply.github.com>
2025-07-18 19:53:49 -0300

    cab mudança

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Cabecalho from '$lib/componentes/Cabecalho.svelte';
 	import '../app.css';
-	import Cabecalho from "$lib/componentes/Cabecalho.svelte";
 	import Rodape from "$lib/componentes/Rodape.svelte";
 
 	let { children } = $props();
